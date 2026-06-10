ff76ae6bef6b9ae63459faeaa7044bcf1eae1bd8
DiegoGarcia271004 <00088023@uca.edu.sv>
2026-06-09 19:05:04 -0600

    fix: Se quita la respuesta en el login que devolvia el token, ya que no se usa

--- backend/WarehouseInventory/src/main/java/org/example/warehouseinventory/auth/api/controller/AuthController.java
+++ backend/WarehouseInventory/src/main/java/org/example/warehouseinventory/auth/api/controller/AuthController.java
@@ -6,20 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.warehouseinventory.auth.application.service.AuthService;
 import org.example.warehouseinventory.auth.domain.dto.request.LoginRequest;
 import org.example.warehouseinventory.auth.domain.dto.request.RegisterRequest;
-import org.example.warehouseinventory.auth.domain.dto.response.CsrfTokenResponse;
-import org.example.warehouseinventory.auth.domain.entity.User;
-import org.example.warehouseinventory.auth.infraestructure.repository.UserRepository;
 import org.example.warehouseinventory.shared.api.BaseController;
-import org.example.warehouseinventory.shared.api.exception.BusinessRuleViolationException;
 import org.example.warehouseinventory.shared.domain.GeneralResponse;
-import org.example.warehouseinventory.shared.utils.config.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,8 +24,8 @@ public class AuthController extends BaseController {
 
     @PostMapping("/login")
     public ResponseEntity<GeneralResponse> login(@Valid @RequestBody LoginRequest req, HttpServletResponse res) {
-        String csrfToken = authService.login(req, res);
-        return buildResponse("Login successful", HttpStatus.OK, new CsrfTokenResponse(csrfToken));
+        authService.login(req, res);
+        return buildResponse("Login successful", HttpStatus.OK, null);
     }
 
 
